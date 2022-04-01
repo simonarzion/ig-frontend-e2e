8e446bc4340ecfb8fba890c89075387433cc743c
simonarzion <simon.villaverde@arzion.com>
2022-04-01 13:13:23 -0300

    tests updated

--- cypress/integration/register.spec.js
+++ cypress/integration/register.spec.js
@@ -1,17 +1,17 @@
-describe('register errors', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000/auth/signup');
-    });
+describe("register errors", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/auth/signup");
+  });
 
-    it('should throw email or password incorrect error', () => {
-        cy.registerExistingAccount();
+  it("should throw email or password incorrect error", () => {
+    cy.registerExistingAccount();
 
-        cy.get('#already_exists_account').should('be.visible');
-    });
+    cy.get("#AlreadyExistAccountError").should("be.visible");
+  });
 
-    it('should throw inactive account error', () => {
-        cy.registerInactiveAccount();
+  it("should throw inactive account error", () => {
+    cy.registerInactiveAccount();
 
-        cy.get('#inactive_account').should('be.visible');
-    });
+    cy.get("#InactiveAccountError").should("be.visible");
+  });
 });
