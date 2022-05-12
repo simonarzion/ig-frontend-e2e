ecf4dea4429bfcd1086917a09125a5816ba40d2d
simonarzion <simon.villaverde@arzion.com>
2022-05-12 12:36:31 -0300

    :sparkles: search page spec

--- cypress/integration/search.spec.js
+++ cypress/integration/search.spec.js
@@ -7,7 +7,7 @@ describe("initial tests", () => {
   });
 
   it("should type and search 'pizza' in 'Alabama' with default region 'US'", () => {
-    cy.modalSelectOption(3611, "re");
+    cy.modalSelectOption(3611, "re", "home-hero");
 
     cy.location().should((location) => {
       expect(location.pathname).to.eq("/united-states/find/");
@@ -24,7 +24,22 @@ describe("initial tests", () => {
       expect(location.search).to.eq("?country_iso=AR");
     });
 
-    cy.modalSelectOption(103, "la pla");
+    cy.modalSelectOption(103, "la pla", "home-hero");
+
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq("/argentina/find/");
+      expect(location.search).to.eq("?city_id=1492012&terms=pizza");
+    });
+  });
+
+  it("should use the search from search page", () => {
+    cy.get(".custom-select-trigger").click();
+
+    cy.visit(
+      "http://localhost:5000/argentina/find/?city_id=1492012&terms=pizza"
+    );
+
+    cy.modalSelectOption(103, "la pla", "top-bar");
 
     cy.location().should((location) => {
       expect(location.pathname).to.eq("/argentina/find/");
